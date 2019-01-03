14e5e1071a25de6b5ab00ee7f68d07d2fb951305
Anthony Green <green@moxielogic.com>
2019-01-03 06:51:54 -0500

    Update quicklisp dist version to 2018-12-10

--- 1.0/install.lisp
+++ 1.0/install.lisp
@@ -1,4 +1,4 @@
-(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2018-07-11/distinfo.txt")
+(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2018-12-10/distinfo.txt")
 
 (load "quicklisp.lisp")
 
